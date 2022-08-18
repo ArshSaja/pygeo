78d544bf0ce7e55d2fc6f491cbf505eedf10504c
arshsaja <arshsaja@umich.edu>
2022-08-18 18:49:42 -0400

    debug check

--- pygeo/parameterization/DVGeoMultiParam.py
+++ pygeo/parameterization/DVGeoMultiParam.py
@@ -557,7 +557,7 @@ class DVGeometryMultiParam:
 
         if self.debug:
             print(f"[{self.comm.rank}] finished DVGeo.totalSensitivity")
-
+        print('dIdxDict',dIdxDict)
         return dIdxDict
 
 
@@ -815,12 +815,13 @@ class DVGeometryMultiParam:
                     # This is not the best way to do this
                     # TODO: use sparse storage for these
                     compJ = self.comps[comp].DVGeo.JT[ptSetName].todense().T
-
+                    print('FFD',compJ)
                     # do it (kinda) vectorized
                     jac[ptSet.compMapFlat[comp], dvOffset : dvOffset + nDVComp] = compJ[:, :]
             else:
                 if self.comps[comp].DVGeo.pointSets[ptSetName].jac is not None:
                     compJ = self.comps[comp].DVGeo.pointSets[ptSetName].jac
+                    print('VSP',compJ)
                     # do it (kinda) vectorized
                     jac[ptSet.compMapFlat[comp], dvOffset : dvOffset + nDVComp] = compJ[:, :]
             # increment the offset
