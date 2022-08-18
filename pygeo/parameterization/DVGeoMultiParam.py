51d4a0d22b754641db1005a521cd550374d07d2b
arshsaja <arshsaja@umich.edu>
2022-08-18 16:51:25 -0400

    fixing sensitivity

--- pygeo/parameterization/DVGeoMultiParam.py
+++ pygeo/parameterization/DVGeoMultiParam.py
@@ -547,7 +547,7 @@ class DVGeometryMultiParam:
             # we only do this if this component has at least one DV
             if nDVComp > 0:
                 # this part of the sensitivity matrix is owned by this dvgeo
-                dIdxComp = DVGeo.convertSensitivityToDict(dIdx[:, dvOffset : dvOffset + nDVComp])
+                dIdxComp = DVGeo.convertSensitivityToDict(dIdx[:, dvOffset : dvOffset + nDVComp],out1D=True)
 
                 for k, v in dIdxComp.items():
                     dIdxDict[k] = v
