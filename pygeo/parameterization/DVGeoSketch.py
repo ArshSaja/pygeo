6c4b63e34c2d14c69e2f561003ef9b20bb24537a
arshsaja <arshsaja@umich.edu>
2022-08-30 16:19:54 -0400

    debug check

--- pygeo/parameterization/DVGeoSketch.py
+++ pygeo/parameterization/DVGeoSketch.py
@@ -97,8 +97,13 @@ class DVGeoSketch(BaseDVGeometry):
             The mapped DVs in the same dictionary format
         """
         # first make a copy so we don't modify in place
+        import sys
+        import numpy
+        numpy.set_printoptions(threshold=sys.maxsize)
+        if self.comm.rank==0:
+            print("inDict",inDict)
         inDict = copy.deepcopy(inDict)
-        userVec = inDict.pop(self.DVComposite.name)
+        userVec = inDict[self.DVComposite.name]
         outVec = self.mapVecToDVGeo(userVec)
         outDict = self.convertSensitivityToDict(outVec.reshape(1, -1), out1D=True, useCompositeNames=False)
         # now merge inDict and outDict
@@ -175,7 +180,9 @@ class DVGeoSketch(BaseDVGeometry):
 
             lb = self.convertDictToSensitivity(lb)
             ub = self.convertDictToSensitivity(ub)
-
+            # print(lb,ub)
+            self.DVComposite.lower=lb
+            self.DVComposite.upper=ub
             optProb.addConGroup(
                 f"{self.DVComposite.name}_con",
                 self.getNDV(),
@@ -187,10 +194,10 @@ class DVGeoSketch(BaseDVGeometry):
                 jac={self.DVComposite.name: self.DVComposite.u},
             )
             return
-
-        for dvName in self.DVs:
-            dv = self.DVs[dvName]
-            optProb.addVarGroup(dv.name, dv.nVal, "c", value=dv.value, lower=dv.lower, upper=dv.upper, scale=dv.scale)
+        else:
+            for dvName in self.DVs:
+                dv = self.DVs[dvName]
+                optProb.addVarGroup(dv.name, dv.nVal, "c", value=dv.value, lower=dv.lower, upper=dv.upper, scale=dv.scale)
 
     def writePointSet(self, name, fileName):
         """
